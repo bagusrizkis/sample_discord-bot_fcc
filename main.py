c62c4885dd1857326608b4b59fb0be614b291ac5
Bagus Rizki S <bagus.tho@gmail.com>
2021-01-17 08:23:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+import os
+import requests
+import json
+import random
+
+client = discord.Client()
+
+sad_words = ['sad', 'depressed', 'unhappy', 'angry', 'depressing']
+
+starter_encouragments = [
+  'Cheer up!',
+  'Hang in there',
+  'You are grate person / bot!'
+]
+
+def get_quote():
+  response = requests.get('https://zenquotes.io/api/random')
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " - " + json_data[0]['a']
+  return (quote)
+
+@client.event
+async def on_ready():
+  print('We have login as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  
+  if message.content.startswith('/inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+client.run(os.getenv('TOKEN'))
