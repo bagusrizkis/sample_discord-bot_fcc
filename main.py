88a46f45d44006569fdd6fb2147f0e262b9ce3df
Bagus Rizki S <bagus.tho@gmail.com>
2021-01-17 08:29:41 +0000

    add random reply feature

--- main.py
+++ main.py
@@ -29,8 +29,13 @@ async def on_message(message):
   if message.author == client.user:
     return
   
-  if message.content.startswith('/inspire'):
+  msg = message.content
+  
+  if msg.startswith('/inspire'):
     quote = get_quote()
     await message.channel.send(quote)
+
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(starter_encouragments))
 
 client.run(os.getenv('TOKEN'))
